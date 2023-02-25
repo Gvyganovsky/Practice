5746a334d8612e5ed490626b3ed3064bf88a1892
Gvyganovsky <gvyganovsky@gmail.com>
2023-02-25 15:43:11 +0300

    New theme

--- practice/src/components/header.jsx
+++ practice/src/components/header.jsx
@@ -1,17 +1,19 @@
 import style from '../scss/HomePage.module.scss';
-import { ReactComponent as Basket } from '../assets/icons/basket.svg';
-import { ReactComponent as Logo } from '../assets/icons/logo.svg';
+import { ReactComponent as Basket } from '../assets/icons/Basket.svg';
+import { ReactComponent as Instagram } from '../assets/icons/Instagram.svg';
 
 export const Header = () => {
     return (
         <header className={style.header}>
             <div className={style.header_nav}>
-                <Logo className={style.logo} />
-                <a href="./Page.jsx" className={style.header_text}>Coffe</a>
-                <a href="./Page.jsx" className={style.header_text}>Tea</a>
-                <a href="./Page.jsx" className={style.header_text}>Menu</a>
+                <a href="./Page.jsx" className={style.header_text}>Shop</a>
+                <a href="./Page.jsx" className={style.header_text}>Contact</a>
             </div>
-            <Basket className={style.basket} />
+            <div className={style.header_icons}>
+                <Basket alt="Basket" className={style.basket} />
+                <Instagram alt="Instagram" className={style.instagram} />
+                <div className={style.header_number}>+1 (888) 888-88-88</div>
+            </div>
         </header>
     );
 }
--- practice/src/pages/HomePage.jsx
+++ practice/src/pages/HomePage.jsx
@@ -1,20 +1,13 @@
 import style from '../scss/HomePage.module.scss';
-import { InfoBlock } from '../components/InfoBlock';
-import { Product } from '../components/product';
-import MermaidFreetoedit from '../assets/coffe/Mermaid Freetoedit.png';
-import CaramelFrappuccino from '../assets/coffe/Caramel Frappuccino.png';
-import Frappuccino from '../assets/coffe/Frappuccino.png';
+import { LogoBlock } from '../components/logoBlock';
+import { Products } from '../components/products';
 
 function HomePage() {
     return (
         <section className={style.HomePage}>
-            <InfoBlock />
-            <div className={style.Products}>
-                <Product id='Cf1' img={MermaidFreetoedit} name='Mermaid Freetoedit' Fat='29%' Saturated='65%' gramm='0.5g' />
-                <Product id='Cf2' img={CaramelFrappuccino} name='Caramel Frappuccino' Fat='39%' Saturated='85%' gramm='0.8g' />
-                <Product id='Cf3' img={Frappuccino} name='Frappuccino' Fat='23%' Saturated='45%' gramm='0.3g' />
-                <Product id='Cf1' img={MermaidFreetoedit} name='Mermaid Freetoedit' Fat='29%' Saturated='65%' gramm='0.5g' />
-            </div>
+            <LogoBlock />
+            <Products />
+
 
         </section>
 
