ca83db3e42d6435fd82a9730ef316ac36c3208db
Gvyganovsky <gvyganovsky@gmail.com>
2023-02-25 19:43:46 +0300

    Bicycles

--- practice/src/componentsPC/bicycles.jsx
+++ practice/src/componentsPC/bicycles.jsx
@@ -1,7 +1,29 @@
 import style from '../scss/HomePage.module.scss';
 
-export const Bicycles = (props) => {
+export const Bicycles = () => {
     return (
-        <section></section>
+        <section className={style.bicycles}>
+
+            <div className={style.priceBlock}>
+                <div className={style.bicycles_title}>
+                    SEGWAY Ninebot Kickscooter MAX
+                </div>
+                <div className={style.warrantyBlock}>
+                    <div className={style.warranty_title}>
+                        Add an extended warranty from Extend
+                    </div>
+                    <div className={style.warranty_button}>
+
+                    </div>
+                    <div className={style.warranty_button}>
+
+                    </div>
+                    <div className={style.warranty_button}>
+
+                    </div>
+                </div>
+            </div>
+
+        </section>
     );
 }
