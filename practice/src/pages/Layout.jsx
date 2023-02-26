1842a73be3b8779428813250b3acaa68b07c6a17
Gvyganovsky <gvyganovsky@gmail.com>
2023-02-26 19:56:45 +0300

    Footer

--- practice/src/pages/Layout.jsx
+++ practice/src/pages/Layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
-import { Header } from '../componentsPC/header'
+import { Header } from '../componentsPC/header';
+import { Footer } from '../componentsPC/footer';
 
 function Layout() {
     return (
         <section>
             <Header />
             <Outlet />
+            <Footer />
         </section>
     );
 }
